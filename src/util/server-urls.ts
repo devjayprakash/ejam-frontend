2fd8353973b29dec2e50ae4e5e57a613c76fcfe3
Jay Prakash Pathak <jayprakashpathak@Jays-MacBook-Air.local>
2021-10-31 20:33:41 +0530

    udpated server url

--- src/util/server-urls.ts
+++ src/util/server-urls.ts
@@ -1,5 +1,7 @@
 export const IS_PROD = process.env.NODE_ENV === "production";
-export const BASE_URL = IS_PROD ? "" : "http://localhost:8080/api/v1";
+export const BASE_URL = IS_PROD
+  ? "https://ejam-task.herokuapp.com/api/v1"
+  : "http://localhost:8080/api/v1";
 export const FETCH_ALL_DEPLOYMENTS = BASE_URL + "/deployment/all";
 export const CREATE_NEW_DEPLOYMENT = BASE_URL + "/deployment/create";
 export const DELETE_DEPLOYMENT_URL = BASE_URL + "/deployment/delete";
